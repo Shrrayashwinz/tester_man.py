6aa92c1deb735ac281a9335e7d2e14bee59b9534
Shrrayashwinz <sshrrayash@gmail.com>
2025-09-22 11:48:34 -0400

    added some more crapton

---
+++ Lab3_ssrinivasan3_list.py
@@ -0,0 +1,30 @@
+"""
+Program Name: "Lab3_ssrinivasan_list"
+
+Author: Shrrayash Srinivasan
+
+Purpose of this program: Create a list of strings which are items you will take for your 
+camping trip. Count the number of items in the string. Finally print the sorted list.
+
+Date: September 9, 2025.
+
+"""
+# Make a list of items I need for the camping trip
+camping_stuff = [
+    "Tent",
+    "Sleeping Bag",
+    "Flashlight",
+    "First Aid Kit",
+    "Water Bottle",
+    "Sunscreen",
+    "Insect Repellent",
+    "Camping Stove", 
+    "Maps",
+    "Boots",
+]
+
+# The number of items on the list
+print("The total number of camping items is", len(camping_stuff))
+
+# Sorted list of item
+print(sorted(camping_stuff))
